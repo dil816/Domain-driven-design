17f9b34eb41639d4905731e185746a041501561a
dil816 <Rivindudilochana18329@gmail.com>
2025-08-19 11:30:58 +0530

    added reflection based logging

--- Domain-driven design/Program.cs
+++ Domain-driven design/Program.cs
@@ -3,10 +3,22 @@ using Domain_driven_design;
 
 var user = User.Create("user@mail.com", "First Name", "Last Name");
 
+user.Update("Updated First Name", "Updated Last Name");
+
 foreach (IDomainEvent domainEvent in user.DomainEvents)
 {
-    if (domainEvent is UserRegisteredDomainEvent u)
-    {
-        Console.WriteLine($"User Registered eventId={u.Id}, Time={u.OccurredOnUtc}");
-    }
+    IEnumerable<string> props = domainEvent.GetType().GetProperties()
+        .Select(p => $"{p.Name}={p.GetValue(domainEvent)}");
+
+    Console.WriteLine($"{domainEvent.GetType().Name}: {string.Join(", ", props)}\n");
+
+    //if (domainEvent is UserRegisteredDomainEvent u)
+    //{
+    //    Console.WriteLine($"User Registered eventId={u.Id}, Time={u.OccurredOnUtc}");
+    //}
+
+    //if (domainEvent is UserProfileUpdatedDomainEvent p)
+    //{
+    //    Console.WriteLine($"User Profile Update eventId={p.Id}, Time={p.OccurredOnUtc}");
+    //}
 }
